a7402eb80db553d92495aabf7a4d7c8d76fdc1fc
Astrid Nino <astrid.nio@gmail.com>
2022-08-04 19:12:38 -0500

    format style fix

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -1,5 +1,13 @@
-describe('Browser Action', () => {
-	it('should load correct URL', () => {
-		cy.visit('https://example.com/', { timeout: 10000 });
+describe('Browser actions', () => {
+	it('should load books website', () => {
+		cy.visit('https://books.toscrape.com/index.html', { timeout: 10000 });
+		cy.url().should('include', 'index.html');
+	});
+	it('should click on travel category', () => {
+		cy.get('a').contains('Travel').click();
+		cy.get('h1').contains('Travel');
+	});
+	it('should display the correct number of books', () => {
+		cy.get('.product_pod').its('length').should('eq', 11);
 	});
 });
